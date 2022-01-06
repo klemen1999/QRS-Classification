d1c11ba831afca9782bdccf8ab23b57153237de4
klemen1999 <klemenskrlj8@gmail.com>
2022-01-06 01:11:06 -0800

    added majority voting

--- QRSClassify.m
+++ QRSClassify.m
@@ -10,8 +10,18 @@ function [classifications] = QRSClassify(record, beats, Fs)
         classifications = NaN;
         return
     end
-    threshold = (1/length(averageBeat)) * sum(abs(averageBeat-(averageBeat*0.2)));
-
+    
+    %threshold = max(abs(averageBeat-(averageBeat*0.5)));
+    
+    % For majority voting:
+    % using d1 best static threshold 0.45
+    threshold1 = (1/length(averageBeat)) * sum(abs(averageBeat-(averageBeat*0.45)));
+    % using d2 best static threshold 0.47
+    threshold2 = sqrt((1/length(averageBeat))*sum(abs(averageBeat-(averageBeat*0.47))).^2);
+    % usign dInf best static threshold 0.5
+    threshold3 = max(abs(averageBeat-(averageBeat*0.5)));
+    thresholds = [threshold1, threshold2, threshold3];
+    
     limLower = floor(Fs*0.06);
     limUpper = round(Fs*0.1);
     fpPoints = beats(:,1);
@@ -20,12 +30,14 @@ function [classifications] = QRSClassify(record, beats, Fs)
         currFp = fpPoints(i);
         if currFp+limUpper <= length(fsig)
             currBeat = fsig(currFp-limLower:currFp+limUpper);
-            [currLabel, threshold] = classifyBeat(currBeat, averageBeat, threshold);
+            %currLabel = classifyBeat(currBeat, averageBeat, threshold);
+            currLabel = classifyBeatMV(currBeat, averageBeat, thresholds);
             classifications = [classifications, currLabel];
         else
             currBeat = fsig(currFp-limLower:end);
             tempAverageBeat = averageBeat(1:length(currBeat));
-            [currLabel, threshold] = classifyBeat(currBeat, tempAverageBeat, threshold);
+            %currLabel = classifyBeat(currBeat, tempAverageBeat, threshold);
+            currLabel = classifyBeatMV(currBeat, tempAverageBeat, thresholds);
             classifications = [classifications, currLabel];
         end
     end
@@ -51,15 +63,29 @@ function [averageBeat] = getAverageBeat(sig, beats, Fs)
     averageBeat = averageBeat ./ length(fpPoints);
 end
 
-function [class, newThreshold] = classifyBeat(currBeat, averageBeat, threshold)
+function [class] = classifyBeat(currBeat, averageBeat, threshold)
     N = length(averageBeat);
-    d1 = (1/N) * sum(abs(currBeat-averageBeat));
-    newThreshold = threshold;
-    %d2 = sqrt((1/N)*sum(abs(currBeat-averageBeat)).^2);
-    %dInf = max(abs(currBeat-averageBeat));
-    if d1 > threshold
+    %dist = (1/N) * sum(abs(currBeat-averageBeat)); %d1
+    %dist = sqrt((1/N)*sum(abs(currBeat-averageBeat)).^2); %d2
+    dist = max(abs(currBeat-averageBeat)); %dInf
+    if dist > threshold
         class = 1; % V
     else
         class = 0; % N
     end
+end
+
+function [class] = classifyBeatMV(currBeat, averageBeat, thresholds)
+    N = length(averageBeat);
+    dist1 = (1/N) * sum(abs(currBeat-averageBeat)); %d1
+    dist2 = sqrt((1/N)*sum(abs(currBeat-averageBeat)).^2); %d2
+    dist3 = max(abs(currBeat-averageBeat)); %dInf
+    distances = [dist1, dist2, dist3];
+    voteResult = sum(distances>thresholds);
+    if voteResult >= 2 
+        class = 1;
+    else
+        class = 0;
+    end
+
 end
